09934b380d50db341bf8c62c92751971661ca8a8
Call4han <ccasarider@gmail.com>
2022-10-21 16:33:14 -0500

    fix(PlayerQuitEvent): Changed the event to the needed event

--- plugin/src/main/java/team/devblook/blootils/listeners/PlayerLeave.java
+++ plugin/src/main/java/team/devblook/blootils/listeners/PlayerLeave.java
@@ -1,21 +1,23 @@
 package team.devblook.blootils.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerLeave implements Listener {
 
-    public void onJoin(PlayerJoinEvent event){
+    public void onJoin(PlayerQuitEvent event){
 
         //TODO: create a new path to configure the leave message and motd when a player leaves the server
         Player player = event.getPlayer();
 
-        event.setJoinMessage(ChatColor.translateAlternateColorCodes('&', "&8[&4-] &C"
+         event.setQuitMessage(ChatColor.translateAlternateColorCodes('&', "&8[&4-] &C"
                 + player.getName() + " &fhas left the server."));
-        player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 10, 5);
+        Bukkit.getOnlinePlayers().forEach(players -> players.playSound(players.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1));
 
     }
 }
