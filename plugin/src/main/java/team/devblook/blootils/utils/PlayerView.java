81dd8f4ccbc41aa6cd5807bd941c5b8ff6abf452
Call4han <ccasarider@gmail.com>
2022-10-23 20:08:28 -0500

    new(PlayerView): add features for players and more.

---
+++ plugin/src/main/java/team/devblook/blootils/utils/PlayerView.java
@@ -0,0 +1,41 @@
+package team.devblook.blootils.utils;
+
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+
+
+
+public class PlayerView {
+    public static void viewBlockActual(Player player,Integer line,String text) {
+        Block block = player.getTargetBlockExact(5);
+
+
+            Sign sign = (Sign) block.getState();
+            String[] lines = sign.getLines();
+            sign.setLine(line,text);
+            sign.update();
+
+
+    }
+    public static void clearAll(Player p){
+        Block block = p.getTargetBlockExact(5);
+        Sign sign = (Sign) block.getState();
+        String[] lines = sign.getLines();
+        sign.setLine(0,"");
+        sign.setLine(1,"");
+        sign.setLine(2,"");
+        sign.setLine(3,"");
+        sign.update();
+    }
+    public static String returnText(Player p,int line){
+        Block block = p.getTargetBlockExact(5);
+        Sign sign = (Sign) block.getState();
+        String text = sign.getLine(line);
+
+        return text;
+    }
+
+
+}
+
