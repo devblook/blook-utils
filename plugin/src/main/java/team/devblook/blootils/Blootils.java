503a43338253b6863628ca7bda4b9b256e024c84
Call4han <ccasarider@gmail.com>
2022-10-27 17:37:53 -0500

    feat(Economy): Add Vault-Api.

--- plugin/src/main/java/team/devblook/blootils/Blootils.java
+++ plugin/src/main/java/team/devblook/blootils/Blootils.java
@@ -1,29 +1,60 @@
 package team.devblook.blootils;
 
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import team.devblook.blootils.command.*;
 
+import team.devblook.blootils.listeners.InteractListener;
 import team.devblook.blootils.managers.UsersData;
-
 public class Blootils extends JavaPlugin {
-
-
+    private static Economy econ = null;
+    private static Blootils instance;
     private UsersData usersData;
 
     @Override
     public void onEnable() {
-
+        instance = this;
+        if (!setupEconomy() ) {
+            getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
         this.usersData = new UsersData();
         this.getCommand("fly").setExecutor(new FlyCommand());
         this.getCommand("feed").setExecutor(new FeedCommand());
         this.getCommand("heal").setExecutor(new HealCommand());
         this.getCommand("sign").setExecutor(new SignCommand());
+        this.getServer().getPluginManager().registerEvents(new InteractListener(),this);
 
+
+
+    }
+
+
+    public static Blootils getInstance() {
+        return instance;
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public static Economy getEcon() {
+        return econ;
     }
 
     public UsersData getUsersData() {
