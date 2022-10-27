2f72fd60ac4ca2b361a3e07bfed53af965a92ecc
Call4han <ccasarider@gmail.com>
2022-10-27 17:24:57 -0500

    feat(SignManager): Add the sign Manager.

---
+++ plugin/src/main/java/team/devblook/blootils/managers/SignManager.java
@@ -0,0 +1,21 @@
+package team.devblook.blootils.managers;
+
+
+import org.bukkit.block.Sign;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public abstract class SignManager{
+    protected Sign sign;
+    public SignManager(Sign sign) {
+        this.sign = sign;
+    }
+
+    public abstract String typeSign();
+    public abstract int line();
+    public abstract String text();
+    public abstract String permission();
+    public abstract void handleSign(PlayerInteractEvent e);
+
+
+
+}
