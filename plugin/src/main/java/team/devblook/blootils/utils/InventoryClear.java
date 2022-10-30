54b82cf6f78dd7092ffe4952c7708c0f2252dca8
Call4han <ccasarider@gmail.com>
2022-10-30 17:57:49 -0500

    feat(InventoryClear): add class for delete inventory when this is current clicked the sign clear

---
+++ plugin/src/main/java/team/devblook/blootils/utils/InventoryClear.java
@@ -0,0 +1,15 @@
+package team.devblook.blootils.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class InventoryClear {
+
+    public static void clearInventory(Player player) {
+        Inventory inventory = Bukkit.createInventory(null, 54, "Clear Inventory");
+        player.openInventory(inventory);
+    }
+
+}
+
