d5805a2fb47abf8454f90ea707d77ada1f27e036
ItzDavidVZLA <86946953+ItzDavidVZLA@users.noreply.github.com>
2022-10-31 23:52:04 -0300

    fix(PlayerLeave): fixed a bug in which the exit message was not sent to the player

--- plugin/src/main/java/team/devblook/blootils/listeners/PlayerLeave.java
+++ plugin/src/main/java/team/devblook/blootils/listeners/PlayerLeave.java
@@ -4,20 +4,22 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerLeave implements Listener {
 
+    @EventHandler
     public void onJoin(PlayerQuitEvent event){
 
         //TODO: create a new path to configure the leave message and motd when a player leaves the server
         Player player = event.getPlayer();
 
-         event.setQuitMessage(ChatColor.translateAlternateColorCodes('&', "&8[&4-] &C"
+         event.setQuitMessage(ChatColor.translateAlternateColorCodes('&', "&8[&4-] &c"
                 + player.getName() + " &fhas left the server."));
-        Bukkit.getOnlinePlayers().forEach(players -> players.playSound(players.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1));
+         Bukkit.getOnlinePlayers().forEach(players -> players.playSound(players.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1));
 
     }
 }
