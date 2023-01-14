7088b754b44fa8bf9a0d819ae49ca68a82e84fa5
Call4han <ccasarider@gmail.com>
2023-01-13 20:35:42 -0500

    fix(spectatorcommand): fix spectator command if statement

--- plugin/src/main/java/team/devblook/blootils/command/SpectatorCommand.java
+++ plugin/src/main/java/team/devblook/blootils/command/SpectatorCommand.java
@@ -14,14 +14,14 @@ public class SpectatorCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player player) {
             if (!player.hasPermission("blootils.command.spectato") || !player.isOp()) {
+                player.sendMessage(ChatColor.RED + "You don't have the enough permissions to access this command.");
+                player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1, 1);
+                return true;
+            } else {
                 player.setGameMode(GameMode.SPECTATOR);
                 player.sendMessage(ChatColor.GRAY + "Gamemode set to " + ChatColor.BLUE + "spectator"
                         + ChatColor.GRAY + ".");
                 player.playSound(player.getLocation(), Sound.BLOCK_LEVER_CLICK, 1, 1);
-                return true;
-            } else {
-                player.sendMessage(ChatColor.RED + "You don't have the enough permissions to access this command.");
-                player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1, 1);
                 return true;
             }
         }
