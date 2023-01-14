200311ca469ed89a738b5027a54c8232c54db63d
Call4han <ccasarider@gmail.com>
2023-01-13 19:42:18 -0500

    fix(enderchestcommand): sync command with gradle plugin and fix if statement issues.

--- plugin/src/main/java/team/devblook/blootils/command/EnderChestCommand.java
+++ plugin/src/main/java/team/devblook/blootils/command/EnderChestCommand.java
@@ -12,14 +12,14 @@ public class EnderChestCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player player) {
-            if (!player.hasPermission("blootils.enderchest") || player.isOp()) {
+            if (!player.hasPermission("blootils.command.enderchest") || !player.isOp()) {
+                player.sendMessage(ChatColor.RED + "You don't have the enough permissions to access this command.");
+                player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1, 1);
+                return true;
+            } else {
                 player.sendMessage(ChatColor.GREEN + "Opening Enderchest...");
                 player.openInventory(player.getEnderChest());
                 player.playSound(player.getLocation(), Sound.BLOCK_ENDER_CHEST_OPEN, 1, 1);
-                return true;
-            } else {
-                player.sendMessage(ChatColor.RED + "You don't have the enough permissions to access this command.");
-                player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1, 1);
                 return true;
             }
         }
